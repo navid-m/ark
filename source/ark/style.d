39ae90ddcdc89bde6a144e8060404c41baa0ccb3
Navid M <navidmomtahen@gmail.com>
2025-06-03 12:49:46 +0100

    docs

--- source/ark/style.d
+++ source/ark/style.d
@@ -2,15 +2,37 @@ module ark.style;
 
 template ArkStyle()
 {
+    /** 
+     * Whether to enable color or not.
+     *
+     * Params:
+     *   enable = Color or no color
+     */
     static void enableColor(bool enable = true)
     {
         colorEnabled = enable;
     }
 
+    /** 
+     * Colorize some text using corresponding ANSI escpae code.
+     *
+     * Params:
+     *   text = Text to colorize
+     *   color = Color to use
+     * Returns: 
+     */
     private static string colorize(string text, Color color) => colorEnabled ? (
         color ~ text ~ Color.RESET
     ) : text;
 
+    /** 
+     * Stylize some text.
+     *
+     * Params:
+     *   text = The text
+     *   style = The style to use (e.g. underline, bold, etc...)
+     * Returns: 
+     */
     private static string stylize(string text, Style style) => colorEnabled ? (
         style ~ text ~ Style.RESET
     ) : text;
